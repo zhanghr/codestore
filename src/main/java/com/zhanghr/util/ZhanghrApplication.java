025f54c3d99fb8da6d3125dd94faedb09cee273a
张华荣 <429763991@qq.com>
2020-01-14 16:13:33 +0800

    init

---
+++ src/main/java/com/zhanghr/util/ZhanghrApplication.java
@@ -0,0 +1,13 @@
+package com.zhanghr.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhanghrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhanghrApplication.class, args);
+	}
+
+}
